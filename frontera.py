b17a3bdd0157a8518d3285ea3a7ee78c1fbddf46
Facundo <facundolotobattan@gmail.com>
2025-06-06 00:25:09 -0300

    Fix data download parameters and enhance portfolio visualization with efficient frontier plot

--- frontera.py
+++ frontera.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 tickers = ['GGAL', 'YPF', 'BBAR', 'BMA','CEPU','CRESY','EDN','IRS','LOMA','PAM','SUPV','TEO','TGS','TS','TX']
 start = datetime.now() - timedelta(days=10*365)
 end = datetime.now()
-df = yf.download(tickers, start, end)
+df = yf.download(tickers, start, end,auto_adjust=False)
 returns = df['Close'].pct_change().dropna()
 
 #Variables
@@ -29,6 +29,30 @@ port = rp.Portfolio(returns = returns)
 port.assets_stats(method_mu=method_mu, method_cov=method_cov )
 w = port.optimization(model=model, rm=rm , obj=obj , rf=rf , l=l , hist=hist)
 
-#Plot Pie Chart
-ax = rp.plot_pie(w, title='Portfolio Optimo Ponderacion', cmap = 'tab20')
+# Generate efficient frontier weights (you might need to adjust the number of points)
+ws = port.efficient_frontier(model=model,points=100)
+
+# Plot efficient frontier
+label = 'Max Risk Adjusted Return Portfolio'
+mu = port.mu
+cov = port.cov
+
+ax = rp.plot_frontier(w_frontier=ws,
+                      mu=mu,
+                      cov=cov,
+                      returns=returns,
+                      rm=rm,
+                      rf=0,
+                      alpha=0.05,
+                      cmap='viridis',
+                      w=w,
+                      label=label,
+                      marker='*',
+                      s=16,
+                      c='r',
+                      height=6,
+                      width=10,
+                      t_factor=252,
+                      ax=None)
+
 plt.show()
